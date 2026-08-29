611a334f763f49f8ac106189ee672e320ef8abde
edemsokpe <145849175+edemsokpe@users.noreply.github.com>
2026-08-29 23:53:08 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 const m=document.querySelector('.menu'),n=document.querySelector('.navlinks');if(m)m.onclick=()=>n.classList.toggle('show');document.querySelectorAll('[data-year]').forEach(x=>x.textContent=new Date().getFullYear());
+
+const videos = document.querySelectorAll(".slide-video");
+let current = 0;
+
+setInterval(() => {
+
+    videos[current].pause();
+    videos[current].classList.remove("active");
+
+    current = (current + 1) % videos.length;
+
+    videos[current].currentTime = 0;
+    videos[current].classList.add("active");
+    videos[current].play();
+
+}, 10000);
